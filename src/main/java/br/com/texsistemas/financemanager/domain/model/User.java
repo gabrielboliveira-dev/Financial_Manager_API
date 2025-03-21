1e4e3130cf5ddb9f02324189c43cd5b44f4fce82
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-03-21 18:38:51 -0300

    feat: new User

--- src/main/java/br/com/texsistemas/financemanager/domain/model/User.java
+++ src/main/java/br/com/texsistemas/financemanager/domain/model/User.java
@@ -5,9 +5,9 @@ import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
