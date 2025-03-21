c257346563ada78ac8d2aa17ee10269ab0db34f6
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-03-21 18:26:25 -0300

    feat: new BusinessException

--- src/main/java/br/com/texsistemas/financemanager/domain/exception/BusinessException.java
+++ src/main/java/br/com/texsistemas/financemanager/domain/exception/BusinessException.java
@@ -1,4 +1,12 @@
 package br.com.texsistemas.financemanager.domain.exception;
 
-public class BusinessException {
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
