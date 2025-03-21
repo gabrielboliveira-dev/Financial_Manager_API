6f0c9b7b27f8e41bc2f220cd17f504425456bf92
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-03-21 18:27:47 -0300

    feat: add User

--- src/main/java/br/com/texsistemas/financemanager/domain/model/User.java
+++ src/main/java/br/com/texsistemas/financemanager/domain/model/User.java
@@ -2,8 +2,12 @@ package br.com.texsistemas.financemanager.domain.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -20,17 +24,32 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @NotBlank
+    @Size(max = 100)
     @Column(nullable = false, length = 100)
     private String name;
 
+    @NotBlank
+    @Email
+    @Size(max = 150)
     @Column(nullable = false, unique = true, length = 150)
     private String email;
 
+    @NotBlank
+    @CPF
+    @Size(max = 11)
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @NotBlank
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private Boolean ativo;
 
     public void setPassword(String rawPassword) {
         this.password = new BCryptPasswordEncoder().encode(rawPassword);
