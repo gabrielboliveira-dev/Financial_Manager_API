c27a1a05b08fa03b3da155f4df7781585aa3d621
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-03-21 18:20:24 -0300

    feat: new UserRepository

--- src/main/java/br/com/texsistemas/financemanager/domain/repository/UserRepository.java
+++ src/main/java/br/com/texsistemas/financemanager/domain/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +14,16 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Transactional(readOnly = true)
     Optional<User> findByEmail(String email);
+
+    @Transactional(readOnly = true)
+    Optional<User> findByEmailAndAtivoTrue(String email);
+
+    @Transactional(readOnly = true)
+    Optional<User> findByIdAndAtivoTrue(UUID id);
+
+    @Transactional(readOnly = true)
+    Optional<User> findByCpf(String cpf);
+
+    @Transactional(readOnly = true)
+    List<User> findByAtivoTrue();
 }
