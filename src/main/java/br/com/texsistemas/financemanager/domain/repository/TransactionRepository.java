d609e65d205c6f8fa671a9cfb5102d9b6a69a374
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-03-22 13:47:52 -0300

    refactor: UserRepository.java and TransactionRepository.java

--- src/main/java/br/com/texsistemas/financemanager/domain/repository/TransactionRepository.java
+++ src/main/java/br/com/texsistemas/financemanager/domain/repository/TransactionRepository.java
@@ -11,12 +11,19 @@ import java.util.UUID;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
+    //Busca todas as transações associadas a uma determinada conta.
     @Transactional(readOnly = true)
     List<Transaction> findByAccountId(UUID accountId);
 
+    //Busca todas as transações pertencentes a uma determinada categoria
     @Transactional(readOnly = true)
     List<Transaction> findByCategoryId(UUID categoryId);
 
+    //busca todas as transações de um determinado tipo (por exemplo, "RECEITA" ou "DESPESA").
     @Transactional(readOnly = true)
     List<Transaction> findByType(String type);
+
+    //Busca todas as transações associadas a um determinado utilizador.
+    @Transactional(readOnly = true)
+    List<Transaction> findByAccount_User_Id(UUID userId);
 }
--- src/main/java/br/com/texsistemas/financemanager/domain/repository/UserRepository.java
+++ src/main/java/br/com/texsistemas/financemanager/domain/repository/UserRepository.java
@@ -12,18 +12,25 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    //A operação será executada numa transação e que apenas operações de leitura serão realizadas, otimizando o desempenho.
+    //Busca um utilizador no banco de dados pelo seu endereço de endereço eletrónico.
+    //Retorna um Optional<User> para lidar com o caso em que nenhum utilizador com o endereço eletrónico fornecido seja encontrado.
     @Transactional(readOnly = true)
     Optional<User> findByEmail(String email);
 
+    //Busca um utilizador pelo seu endereço de endereço eletrónico, mas apenas se o campo 'ativo' for verdadeiro.
     @Transactional(readOnly = true)
     Optional<User> findByEmailAndAtivoTrue(String email);
 
+    //Busca um utilizador pelo seu ‘ID’, mas apenas se o campo 'ativo' for verdadeiro.
     @Transactional(readOnly = true)
     Optional<User> findByIdAndAtivoTrue(UUID id);
 
+    //Busca um utilizador pelo seu CPF.
     @Transactional(readOnly = true)
     Optional<User> findByCpf(String cpf);
 
+    //busca todos os utilizadores que possuem o campo 'ativo' como verdadeiro e retorna uma lista deles.
     @Transactional(readOnly = true)
     List<User> findByAtivoTrue();
 }
